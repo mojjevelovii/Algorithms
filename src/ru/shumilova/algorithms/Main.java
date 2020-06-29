644a7b158afdbd6b1c738a9d5c4676388a753b23
Veronika Shumilova <mojjevelovii@gmail.com>
2020-06-29 11:30:25 +0300

    Импорт проекта с урока

--- src/ru/shumilova/algorithms/Main.java
+++ src/ru/shumilova/algorithms/Main.java
@@ -1,8 +1,15 @@
 package ru.shumilova.algorithms;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        int[] arr; // int arr[]
+        arr = new int[5];
+        int[] arr2 = {1, 2, 3, 4};
+
+        Array a0 = new Array(1, -2, -3, -4, 5);
+        a0.sortBubble();
+        System.out.println(a0);
     }
 }
