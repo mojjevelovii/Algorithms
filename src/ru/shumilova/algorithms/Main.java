bb4e479742168787ac953c969b078b40261ad0dc
Veronika Shumilova <mojjevelovii@gmail.com>
2020-07-02 12:33:51 +0300

    задача 1. Создать программу, которая переворачивает вводимые строки (читает справа налево).

--- src/ru/shumilova/algorithms/Main.java
+++ src/ru/shumilova/algorithms/Main.java
@@ -1,8 +1,29 @@
 package ru.shumilova.algorithms;
 
+import java.util.Stack;
+
 public class Main {
+    /*
+    1. Создать программу, которая переворачивает вводимые строки (читает справа налево).
+    2. Создать класс для реализации дека.
+    3. Реализовать класс с приоритетной очередью
+    */
+
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(stringReverse("abc"));
+    }
+
+    private static String stringReverse(String string) {
+        Stack<Character> chars = new Stack<>();
+        for (char ch : string.toCharArray()) {
+            chars.push(ch);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!chars.empty()){
+            stringBuilder.append(chars.pop());
+        }
+        return stringBuilder.toString();
     }
 }
