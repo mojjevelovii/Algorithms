9d278818df1d465dcfcc4351aa9b02e96ae5a11d
Veronika Shumilova <mojjevelovii@gmail.com>
2020-07-06 20:19:24 +0300

    перенос кода с урока

--- src/ru/shumilova/algorithms/Main.java
+++ src/ru/shumilova/algorithms/Main.java
@@ -1,8 +1,27 @@
 package ru.shumilova.algorithms;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        RelatedList rl = new RelatedList();
+        rl.push(new Cat(2, "cat1"));
+        rl.push(new Cat(3, "cat2"));
+        rl.push(new Cat(4, "cat3"));
+        rl.push(new Cat(5, "cat4"));
+        rl.push(new Cat(6, "cat5"));
+        System.out.println(rl);
+        System.out.println(rl.pop());
+        System.out.println(rl);
     }
+    //reset();
+    //next();
+    //getCurrent();
+    //hasNext();
+    //atEnd();
+    //insertAfter();
+    //*insertBefore();
+    //deleteCurrent();
+
+    //1. Реализовать класс итератора для двусвязного списка.
+    //2. Реализовать двусвязный список
+
 }
