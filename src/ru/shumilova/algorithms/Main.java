a591105915b0854346420c5024ebcb8c5ef33d59
Veronika Shumilova <mojjevelovii@gmail.com>
2020-07-09 22:08:06 +0300

    Задача 1. Ханойские башни

--- src/ru/shumilova/algorithms/Main.java
+++ src/ru/shumilova/algorithms/Main.java
@@ -3,6 +3,13 @@ package ru.shumilova.algorithms;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        hanoi(5, 1, 2, 3);
+    }
+
+    static void hanoi(int count, int one, int two, int three) {
+        if (count == 0) return;
+        hanoi(count - 1, one, three, two);
+        System.out.println(one + "-->" + two);
+        hanoi(count - 1, three, two, one);
     }
 }
